a1593facbe5b788a3bba47ce859d40412aba0eba
peternjeru <maxp555@gmail.com>
2019-10-10 09:26:42 +0300

    Added 3rd Party's **RequestID** to Signature Data

--- index.php
+++ index.php
@@ -7,26 +7,27 @@ require './vendor/autoload.php';
 //sample B2C callback
 $callbackRequest = "{\"ResultCode\":\"0\",
 \"ResultDesc\":\"The service request is processed successfully.\",
-\"RequestID\":\"5057242165947576\",
-\"MpesaRequestID\":\"AG_20191008_00007e2510a9a3639b00\",
-\"MpesaTransactionID\":\"NJ81HBNKJV\",
+\"RequestID\":\"3113319730816318\",
+\"MpesaRequestID\":\"AG_20191010_00004342ca57f86f0572\",
+\"MpesaTransactionID\":\"NJA6HBNZP0\",
 \"ReferenceData\":[],
-\"ResultData\":
-	{\"TransactionAmount\":10,
+\"ResultData\":{
+	\"TransactionAmount\":10,
 	\"ReceiverPartyName\":\"254796778039 - Peter Test\",
 	\"IsReceiverRegistered\":\"Y\",
-	\"UtilityAccBalance\":1004326077.29,
-	\"WorkingAccBalance\":299183729,
-	\"TransactionCompleteTime\":\"08.10.2019 09:52:32\",
-	\"Signature\":\"hqRWjWRJPCqlqG3FuEj+y8k0Guse4ZSIIYSIYduMDH8eMZJtU2ZqVzezjiPhLCjsJzVvaKrPiJNn2irmeWuxrCroehXowD5RpjcuqpJZ1qJxKXdVtZ90Np4ICqTwMHUM\/8NbCXmFU\/Gz5UddFhAtQkaNHzY9pZiLCPbcHv4n500=\",
-	\"PublicKey\":\"-----BEGIN RSA PUBLIC KEY-----\r\nMIGJAoGBAMJZZLoxxGE\/lQgXnUA59L9d35IIveKd8W0peiJbJxTpnk0RufFfgnIP\r\nx5GWVCJvcy2C4x6Bak21J9jEc5eaEcvGTf8SMPcf0qc3dtbTtbGxV7BW\/vhZimbP\r\nLI9+yxFszym3+A4zd0uOfr37juwRedtr6UufuTwbZzFheC9eEVYpAgMBAAE=\r\n-----END RSA PUBLIC KEY-----\"}
+	\"UtilityAccBalance\":1004321692.29,
+	\"WorkingAccBalance\":299178669,
+	\"TransactionCompleteTime\":\"10.10.2019 09:29:09\",
+	\"Signature\":\"HA3DsXbWYdl7WnBURDyPxxNlrP73Jc1ai+44GIzs0kPLkmke75Zlp4K+9KPffMpykSpqhs686SMURhUgwkrOpm51D8airZQ3VzXwgPYe3y53gTgFxNknORhXgAf9yUeMJx5pwsy1Sak0WYh09ShhiqVJ\/0KqAeaWXEUO\/MGMkfY=\",
+	\"PublicKey\":\"-----BEGIN RSA PUBLIC KEY-----\r\nMIGJAoGBANaKQyGjaID9xw8NtxbLolXfInrzDZ5DLPlW7hW54p051FKskKo94AZs\r\n+m\/ARuR3x0aLb7awJIeuY1yJLeb6cwLg+P5mgbQxKdFQnwwuHNZtKpPxZ0KDLnRg\r\n3lSvuDpCr1zoA8XZaHVZwDq9FMvh6eMchRYD7qfMK9jSQY+jT1bBAgMBAAE=\r\n-----END RSA PUBLIC KEY-----\"}
 }";
 
 //use next line if conversion to json object returns null and json_last_error_msg() returns the error "Control character error, possibly incorrectly encoded"
 $callbackRequest = preg_replace('/[[:cntrl:]]/', '', $callbackRequest);
 
 $jsonObj = json_decode($callbackRequest, true);
-$data = $jsonObj["MpesaRequestID"]
+$data = $jsonObj["RequestID"]
+	.$jsonObj["MpesaRequestID"]
 	.$jsonObj["MpesaTransactionID"]
 	.$jsonObj["ResultData"]["TransactionAmount"]
 	.$jsonObj["ResultData"]["ReceiverPartyName"]
